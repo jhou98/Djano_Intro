7ed935155274e2c886e16919c8e2c74dec2099b1
Hou <jackhou@ead.ubc.ca>
2019-01-07 11:00:31 -0800

    added basic pages

--- pages/urls.py
+++ pages/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include 
+# from django.contrib import admin
+from django.urls import path
+## import views from our pages folder 
+from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')), ##include our pages.url
+    # path('admin/', admin.site.urls),
+    ## path(what we want to see on url, what it points to, and name for refer in future)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
